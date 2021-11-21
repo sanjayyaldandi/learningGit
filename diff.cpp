3970b23b877dd98473433ee58d66273629d8c8b2
sanjayyaldandi <sanjayyeldandi123@gmail.com>
2021-11-21 12:43:58 +0530

    initial commit

---
+++ diff.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a = 9;
+    int b = 5;
+    int diff = a - b;
+    cout<<diff;
+    return 0;
+}
---
+++ sum.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a = 9;
+    int b = 5;
+    int sum = a + b;
+    cout<<sum;
+    return 0;
+}
